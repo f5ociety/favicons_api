00d264661c5e8ee6c5be988a4501333f28294fdc
LencoDigitexer <addminuser258@gmail.com>
2022-09-03 18:58:59 +0300

    add requirements.txt and remove debug points

--- server.py
+++ server.py
@@ -12,21 +12,19 @@ services = {
 async def get_favicon_bytes(request):
     service_url = request.match_info.get("service_url", default_service)
     domain_url = request.match_info.get("domain_url", None)
-
+    
     async with ClientSession() as session:
-        print(service_url)
         async with session.get(services[service_url].format(domain_url)) as resp:
-            print(services[service_url].format(domain_url))
             image_bytes = await resp.read()
     return image_bytes
 
 
 async def get_favicon(request):
     image = await get_favicon_bytes(request)
-    return web.Response(body=image, content_type="image/x-ico")
+    return web.Response(body=image, content_type="image/jpeg")
 
 
 app = web.Application()
-app.router.add_get("/{service_url}/{domain_url}", get_favicon, name="choose_service")
-app.router.add_get("/{domain_url}", get_favicon, name="default_service")
+app.add_routes([web.get('/{service_url}/{domain_url}', get_favicon),
+                web.get('/{domain_url}', get_favicon)])
 web.run_app(app, port=4000)
